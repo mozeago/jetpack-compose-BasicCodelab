834acd8c81b2b0c88f6a114c894c3743c668dc33
Moses Asiago <mozzeago@gmail.com>
2021-11-08 06:58:32 +0300

    finished with expandabled list items with animation

--- app/src/main/java/com/mozeago/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/mozeago/basicscodelab/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.mozeago.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mozeago.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -21,17 +31,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    GreetingMyAppCodelLab1()
-                }
+                GreetingMyAppCodelLab1()
             }
         }
     }
 
     @Composable
-    fun OnboardingScreen(onContinuedClicked: () -> Unit) {
-        Surface() {
+    private fun OnboardingScreen(onContinuedClicked: () -> Unit) {
+        Surface {
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
@@ -57,35 +64,61 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun Greeting(name: String) {
+    private fun Greeting(name: String) {
+        Card(
+            backgroundColor = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            CardContent(name)
+        }
+    }
+
+    @Composable
+    private fun CardContent(name: String) {
         var expanded by rememberSaveable {
             mutableStateOf(false)
         }
-        val extraPadding by animateDpAsState(if (expanded) 48.dp else 0.dp)
-        Surface(
-            color = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
-            Row(modifier = Modifier.padding(16.dp)) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(bottom = extraPadding)
-                ) {
-                    Text(text = "Hello,")
-                    Text(text = name)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(12.dp)
+            ) {
+                Text(text = "Hello,")
+                Text(
+                    text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded) {
+                    Text(text = ("Note that only the most commonly used icons are provided by default. You can add a dependency on androidx.compose.material:material-icons-extended to access every icon, but note that due to the very large size of this dependency you should make sure to use R8 / ProGuard to remove unused icons from your application."))
                 }
-                OutlinedButton(onClick = {
-                    expanded = !expanded
-                }) {
-                    Text(if (expanded) "Show Less" else "Show More")
-                }
+            }
+            IconButton(onClick = {
+                expanded = !expanded
+            }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess
+                    else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) stringResource(R.string.showless) else stringResource(
+                        R.string.showmore
+                    )
+                )
             }
         }
     }
 
     @Composable
-    fun GreetingMyAppCodelLab1() {
+    private fun GreetingMyAppCodelLab1() {
         var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
         if (shouldShowOnboarding) {
             OnboardingScreen(onContinuedClicked = { shouldShowOnboarding = false })
@@ -95,7 +128,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    private fun Greetings(names: List<String> = List(1000) { "$it" }) {
         LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
             items(items = names) { name ->
                 Greeting(name = name)
@@ -103,6 +136,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Preview(
+        showBackground = true,
+        widthDp = 320,
+        uiMode = UI_MODE_NIGHT_YES,
+        name = "DefaultPreviewDark"
+    )
     @Preview(showBackground = true, widthDp = 320)
     @Composable
     fun DefaultPreview() {
--- app/src/main/java/com/mozeago/basicscodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/mozeago/basicscodelab/ui/theme/Theme.kt
@@ -5,27 +5,31 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    surface= Blue,
+    onSurface= Navy,
+    primary = Navy,
+    onPrimary  = Chartreuse,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    secondary = Teal200,
+//     Other default colors to override
+//    background = Color.White,
+    surface = Blue,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
-)
+
+    )
 
 @Composable
 fun BasicsCodelabTheme(
